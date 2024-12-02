a1945fc53bfc99f7b0ac6a5853f173aee50d0191
TaysWeb <snelltararobin@gmail.com>
2024-12-02 10:01:59 +0200

    changed the no.of rows shown

--- index.py
+++ index.py
@@ -12,9 +12,9 @@ sheets = ["Gr11 End of Year", "HCT 2024 Survey"]
 data = pd.read_excel(file_path,sheet_name= None)
 
 
-# #Load only the first 105 rows.
+ #Load only the first 500 rows.
 data_subset = pd.read_excel(file_path, nrows=105)
-print(data_subset.head(250))
+print(data_subset.head(500))
 
 # combine multiple Excel files from a folder
 path = "./data/"
@@ -27,7 +27,7 @@ data_combined_ = pd.concat(data.values(), ignore_index=True)
 
 if files:
   data_combined = pd.concat([pd.read_excel(f, index_col=0, parse_dates=[0]) for f in files])
-  print(data_combined.head(100))
+  print(data_combined.head(1000))
   
 else:
     print("No files found to process.") 
